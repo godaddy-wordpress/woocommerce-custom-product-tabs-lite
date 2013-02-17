891a3966f95fe267d09ce940954a94859ecdfe0a
SkyVerge <info@skyverge.com>
2013-02-17 02:16:17 +0000

    WooCommerce Custom Product Tabs Lite version 1.2.0
    WooCommerce 2.0 Compatiblity

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: FoxRunSoftware
 Tags: woocommerce, product tabs
 Requires at least: 3.3
-Tested up to: 3.3
-Stable tag: 1.1.0
+Tested up to: 3.5
+Stable tag: 1.2.0
 
 This plugin extends the WooCommerce e-commerce plugin by allowing a custom product tab to be created with arbitrary content.
 
@@ -12,6 +12,8 @@ This plugin extends the WooCommerce e-commerce plugin by allowing a custom produ
 This plugin extends the [WooCommerce](http://www.woothemes.com/woomember/go?r=130284&redirect=www.woothemes.com/woocommerce/) e-commerce plugin by allowing a custom product tab to be added to product view pages with arbitrary content, which may contain text, html or shortcodes.
 
 The custom tab, if defined, will appear in between the 'additional information' and 'reviews' tabs.
+
+To easily add multiple tabs, share tabs between products, and more features, consider upgrading to the premium [Tab Manager](http://www.woothemes.com/products/woocommerce-tab-manager/)
 
 = Plugin/Theme Support =
 *This plugin should be compatible with any WooCommerce theme and has been tested with the following*
@@ -25,6 +27,7 @@ The custom tab, if defined, will appear in between the 'additional information' 
 
 = More =
 * [Also see my other plugins](http://www.foxrunsoftware.com) or see [my WordPress.org profile page](http://profiles.wordpress.org/foxrunsoftware/)
+* Upgrade to the premium Tab Manager now available from [WooThemes](http://www.woothemes.com/products/woocommerce-tab-manager/)
 
 == Installation ==
 
@@ -37,22 +40,36 @@ The custom tab, if defined, will appear in between the 'additional information' 
 1. Adding a custom tab to a product in the admin
 2. The custom tab displayed on the frontend
 
+== Frequently Asked Questions ==
+
+= Can I add more than tab, or change the order of the tabs? =
+
+This free version does not have that functionality, but now you can with the paid [WooCommerce Tab Manager](http://www.woothemes.com/products/woocommerce-tab-manager/)
+
+= Can I share tab content between more than one tab? =
+
+This free version does not have that functionality, but now you can with the paid [WooCommerce Tab Manager](http://www.woothemes.com/products/woocommerce-tab-manager/)
+
+
 == Changelog ==
 
-= 1.1.0 =
-* Shortcodes enabled for tab content
+= 1.2.0 - 2013.02.16 =
+ * WooCommerce 2.0 Compatiblity
 
-= 1.0.3 =
-* Tab content textarea is larger for easier input
+= 1.1.0 - 2012.04.23 =
+ * Shortcodes enabled for tab content
 
-= 1.0.2 =
-* Fixes an admin bug introduced by the 1.0.1 release (thanks to Cabbola)
+= 1.0.3 - 2012.03.19 =
+ * Tab content textarea is larger for easier input
 
-= 1.0.1 =
-* Fixes T_PAAMAYIM_NEKUDOTAYIM error (thanks daveshine)
+= 1.0.2 - 2012.03.19 =
+ * Fixes an admin bug introduced by the 1.0.1 release (thanks to Cabbola)
 
-= 1.0.0 =
-* Code cleanup
+= 1.0.1 - 2012.03.15 =
+ * Fixes T_PAAMAYIM_NEKUDOTAYIM error (thanks daveshine)
 
-= 0.1 =
-* Initial release
+= 1.0.0 - 2012.03.15 =
+ * Code cleanup
+
+= 0.1 - 2012.03.07 =
+ * Initial release
--- woocommerce-custom-product-tabs-lite.php
+++ woocommerce-custom-product-tabs-lite.php
@@ -1,245 +1,337 @@
 <?php
-/*
-Plugin Name: WooCommerce Custom Product Tabs Lite
-Plugin URI: http://www.foxrunsoftware.net/articles/wordpress/woocommerce-custom-product-tabs/
-Description: Extends WooCommerce to add a custom product view page tab
-Version: 1.1.0
-Author: Justin Stern
-Author URI: http://www.foxrunsoftware.net
-License: GPL2
-*/
-
-/*  Copyright (C) 2012  Justin Stern  (email : justin@foxrunsoftware.net)
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+/**
+ * Plugin Name: WooCommerce Custom Product Tabs Lite
+ * Plugin URI: http://www.foxrunsoftware.net/articles/wordpress/woocommerce-custom-product-tabs/
+ * Description: Extends WooCommerce to add a custom product view page tab
+ * Version: 1.2.0
+ * Author: Justin Stern
+ * Author URI: http://www.foxrunsoftware.net
+ * Tested up to: 3.5
+ *
+ * Copyright: (c) 2012-2013 Justin Stern
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package     WC-Custom-Product-Tabs-Lite
+ * @author      Justin Stern
+ * @category    Plugin
+ * @copyright   Copyright (c) 2012-2013, Justin Stern
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
 
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) exit;
 
-if (!class_exists('WoocommerceCustomProductTabsLite')) :
+// Check if WooCommerce is active and bail if it's not
+if ( ! WoocommerceCustomProductTabsLite::is_woocommerce_active() )
+	return;
+
+/**
+ * The WoocommerceCustomProductTabsLite global object
+ * @name $woocommerce_product_tabs_lite
+ * @global WoocommerceCustomProductTabsLite $GLOBALS['woocommerce_product_tabs_lite']
+ */
+$GLOBALS['woocommerce_product_tabs_lite'] = new WoocommerceCustomProductTabsLite();
 
 class WoocommerceCustomProductTabsLite {
+
 	private $tab_data = false;
-	const VERSION = "1.1.0";
-	
+
+	const VERSION = "1.2.0";
+	const VERSION_OPTION_NAME = 'woocommerce_custom_product_tabs_lite_db_version';
+
 	/**
 	 * Gets things started by adding an action to initialize this plugin once
 	 * WooCommerce is known to be active and initialized
 	 */
 	public function __construct() {
-		add_action( 'woocommerce_init', array(&$this, 'init' ));
-		
 		// Installation
-		if (is_admin() && !defined('DOING_AJAX')) $this->install();
+		if ( is_admin() && ! defined( 'DOING_AJAX' ) ) $this->install();
+
+		add_action( 'woocommerce_init', array( $this, 'init' ) );
 	}
-	
+
+
 	/**
 	 * Init WooCommerce Product Tabs Lite extension once we know WooCommerce is active
 	 */
 	public function init() {
 		// backend stuff
-		add_action('woocommerce_product_write_panel_tabs', array($this, 'product_write_panel_tab'));
-		add_action('woocommerce_product_write_panels',	 array($this, 'product_write_panel'));
-		add_action('woocommerce_process_product_meta',	 array($this, 'product_save_data'), 10, 2);
-		
+		add_action( 'woocommerce_product_write_panel_tabs', array( $this, 'product_write_panel_tab' ) );
+		add_action( 'woocommerce_product_write_panels',     array( $this, 'product_write_panel' ) );
+		add_action( 'woocommerce_process_product_meta',     array( $this, 'product_save_data' ), 10, 2 );
+
 		// frontend stuff
-		add_action('woocommerce_product_tabs',	   array($this, 'custom_product_tabs'),	   25);  // in between the attributes and reviews panels
-		add_action('woocommerce_product_tab_panels', array($this, 'custom_product_tabs_panel'), 25);
-		
+		if ( version_compare( WOOCOMMERCE_VERSION, "2.0" ) >= 0 ) {
+			// WC >= 2.0
+			add_filter( 'woocommerce_product_tabs', array( $this, 'add_custom_product_tabs' ) );
+		} else {
+			// WC < 2.0
+			add_action( 'woocommerce_product_tabs', array( $this, 'custom_product_tabs' ), 25 );
+			// in between the attributes and reviews panels
+			add_action( 'woocommerce_product_tab_panels', array( $this, 'custom_product_tabs_panel' ), 25 );
+		}
+
 		// allow the use of shortcodes within the tab content
 		add_filter( 'woocommerce_custom_product_tabs_lite_content', 'do_shortcode' );
 	}
-	
+
+
+	/** Frontend methods ******************************************************/
+
+
+	/**
+	 * Add the custom product tab
+	 *
+	 * $tabs structure:
+	 * Array(
+	 *   id => Array(
+	 *     'title'    => (string) Tab title,
+	 *     'priority' => (string) Tab priority,
+	 *     'callback' => (mixed) callback function,
+	 *   )
+	 * )
+	 *
+	 * @since 1.2.0
+	 * @param array $tabs array representing the product tabs
+	 * @return array representing the product tabs
+	 */
+	public function add_custom_product_tabs( $tabs ) {
+		global $product;
+
+		if ( $this->product_has_custom_tabs( $product ) ) {
+			foreach ( $this->tab_data as $tab ) {
+				$tabs[ $tab['id'] ] = array(
+					'title'    => $tab['title'],
+					'priority' => 25,
+					'callback' => array( $this, 'custom_product_tabs_panel_content' ),
+					'content'  => $tab['content'],  // custom field
+				);
+			}
+		}
+
+		return $tabs;
+	}
+
+
 	/**
 	 * Write the custom tab on the product view page.  In WooCommerce these are
 	 * handled by templates.
+	 *
+	 * WC < 2.0
 	 */
 	public function custom_product_tabs() {
 		global $product;
-		
-		if($this->product_has_custom_tabs($product)) {
-			foreach($this->tab_data as $tab) {
-				echo "<li><a href=\"#{$tab['id']}\">".__($tab['title'])."</a></li>";
+
+		if ( $this->product_has_custom_tabs( $product ) ) {
+			foreach ( $this->tab_data as $tab ) {
+				echo "<li><a href=\"#{$tab['id']}\">" . __( $tab['title'] ) . "</a></li>";
 			}
 		}
 	}
-	
+
+
 	/**
 	 * Write the custom tab panel on the product view page.  In WooCommerce these
 	 * are handled by templates.
+	 *
+	 * WC < 2.0
 	 */
 	public function custom_product_tabs_panel() {
 		global $product;
-		
-		if($this->product_has_custom_tabs($product)) {
-			foreach($this->tab_data as $tab) {
-				echo '<div class="panel" id="'.$tab['id'].'">';
-				echo '<h2>' . $tab['title'] . '</h2>';
-				echo apply_filters( 'woocommerce_custom_product_tabs_lite_content', $tab['content'], $tab['id'] );
+
+		if ( $this->product_has_custom_tabs( $product ) ) {
+			foreach ( $this->tab_data as $tab ) {
+				echo '<div class="panel" id="' . $tab['id'] . '">';
+				$this->custom_product_tabs_panel_content( $tab['id'], $tab );
 				echo '</div>';
 			}
 		}
 	}
-	
+
+
 	/**
-	 * Lazy-load the product_tabs meta data, and return true if it exists,
-	 * false otherwise
-	 * 
-	 * @return true if there is custom tab data, false otherwise
+	 * Render the custom product tab panel content for the given $tab
+	 *
+	 * $tab structure:
+	 * Array(
+	 *   'title'    => (string) Tab title,
+	 *   'priority' => (string) Tab priority,
+	 *   'callback' => (mixed) callback function,
+	 *   'id'       => (int) tab post identifier,
+	 *   'content'  => (sring) tab content,
+	 * )
+	 *
+	 * @param string $key tab key
+	 * @param array $tab tab data
+	 *
+	 * @param array $tab the tab
 	 */
-	private function product_has_custom_tabs($product) {
-		if($this->tab_data === false) {
-			$this->tab_data = maybe_unserialize( get_post_meta($product->id, 'frs_woo_product_tabs', true) );
-		}
-		// tab must at least have a title to exist
-		return !empty($this->tab_data) && !empty($this->tab_data[0]) && !empty($this->tab_data[0]['title']);
+	public function custom_product_tabs_panel_content( $key, $tab ) {
+		echo '<h2>' . $tab['title'] . '</h2>';
+		echo apply_filters( 'woocommerce_custom_product_tabs_lite_content', $tab['content'], $tab );
+
 	}
-	
+
+
+	/** Admin methods ******************************************************/
+
+
 	/**
 	 * Adds a new tab to the Product Data postbox in the admin product interface
 	 */
 	public function product_write_panel_tab() {
-		echo "<li><a style=\"color:#555555;line-height:16px;padding:9px;text-shadow:0 1px 1px #FFFFFF;\" href=\"#product_tabs\">".__('Custom Tab')."</a></li>";
+		echo "<li class=\"product_tabs_lite_tab\"><a href=\"#woocommerce_product_tabs_lite\">" . __( 'Custom Tab' ) . "</a></li>";
 	}
-	
+
+
 	/**
 	 * Adds the panel to the Product Data postbox in the product interface
 	 */
 	public function product_write_panel() {
-		global $post;  // the product
-		
-		// pull the custom tab data out of the database
-		$tab_data = maybe_unserialize( get_post_meta($post->ID, 'frs_woo_product_tabs', true) );
-		
-		if(empty($tab_data)) {
-			$tab_data[] = array('title' => '', 'content' => '');
+		global $post;
+		// the product
+
+		if ( version_compare( WOOCOMMERCE_VERSION, "2.0.0" ) >= 0 ) {
+			$style = 'padding:5px 5px 5px 28px;background-repeat:no-repeat;background-position:5px 7px;';
+			$active_style = '';
+		} else {
+			$style = 'padding:9px 9px 9px 34px;line-height:16px;border-bottom:1px solid #d5d5d5;text-shadow:0 1px 1px #fff;color:#555555;background-repeat:no-repeat;background-position:9px 9px;';
+			$active_style = '#woocommerce-product-data ul.product_data_tabs li.product_tabs_lite_tab.active a { border-bottom: 1px solid #F8F8F8; }';
 		}
-		
-		foreach($tab_data as $tab) {
+		?>
+		<style type="text/css">
+			#woocommerce-product-data ul.product_data_tabs li.product_tabs_lite_tab a { <?php echo $style; ?> }
+			<?php echo $active_style; ?>
+		</style>
+		<?php
+
+		// pull the custom tab data out of the database
+		$tab_data = maybe_unserialize( get_post_meta( $post->ID, 'frs_woo_product_tabs', true ) );
+
+		if ( empty( $tab_data ) ) {
+			$tab_data[] = array( 'title' => '', 'content' => '' );
+		}
+
+		foreach ( $tab_data as $tab ) {
 			// display the custom tab panel
-			echo '<div id="product_tabs" class="panel woocommerce_options_panel">';
-			woocommerce_wp_text_input( array( 'id' => '_tab_title', 'label' => __('Tab Title'), 'description' => __('Required for tab to be visible'), 'value' => $tab['title'] ) );
-			$this->woocommerce_wp_textarea_input( array( 'id' => '_tab_content', 'label' => __('Content'), 'placeholder' => __('HTML and text to display.'), 'value' => $tab['content'], 'style' => 'width:70%;height:21.5em;' ) );
+			echo '<div id="woocommerce_product_tabs_lite" class="panel wc-metaboxes-wrapper woocommerce_options_panel">';
+			woocommerce_wp_text_input( array( 'id' => '_tab_title', 'label' => __( 'Tab Title' ), 'description' => __( 'Required for tab to be visible' ), 'value' => $tab['title'] ) );
+			$this->woocommerce_wp_textarea_input( array( 'id' => '_tab_content', 'label' => __( 'Content' ), 'placeholder' => __( 'HTML and text to display.' ), 'value' => $tab['content'], 'style' => 'width:70%;height:21.5em;' ) );
 			echo '</div>';
 		}
 	}
-	
-	private function woocommerce_wp_textarea_input( $field ) {
-		global $thepostid, $post;
-		
-		if (!$thepostid) $thepostid = $post->ID;
-		if (!isset($field['placeholder'])) $field['placeholder'] = '';
-		if (!isset($field['class'])) $field['class'] = 'short';
-		if (!isset($field['value'])) $field['value'] = get_post_meta($thepostid, $field['id'], true);
-		
-		echo '<p class="form-field '.$field['id'].'_field"><label for="'.$field['id'].'">'.$field['label'].'</label><textarea class="'.$field['class'].'" name="'.$field['id'].'" id="'.$field['id'].'" placeholder="'.$field['placeholder'].'" rows="2" cols="20"'.(isset($field['style']) ? ' style="'.$field['style'].'"' : '').'">'.esc_textarea( $field['value'] ).'</textarea> ';
-		
-		if (isset($field['description']) && $field['description']) echo '<span class="description">' .$field['description'] . '</span>';
-			
-		echo '</p>';
-	}
-	
+
+
 	/**
 	 * Saves the data inputed into the product boxes, as post meta data
 	 * identified by the name 'frs_woo_product_tabs'
-	 * 
+	 *
 	 * @param int $post_id the post (product) identifier
 	 * @param stdClass $post the post (product)
 	 */
 	public function product_save_data( $post_id, $post ) {
-		
-		$tab_title   = stripslashes($_POST['_tab_title']);
-		$tab_content = stripslashes($_POST['_tab_content']);
-		
-		if(empty($tab_title) && empty($tab_content) && get_post_meta($post_id, 'frs_woo_product_tabs', true)) {
+
+		$tab_title = stripslashes( $_POST['_tab_title'] );
+		$tab_content = stripslashes( $_POST['_tab_content'] );
+
+		if ( empty( $tab_title ) && empty( $tab_content ) && get_post_meta( $post_id, 'frs_woo_product_tabs', true ) ) {
 			// clean up if the custom tabs are removed
-			delete_post_meta($post_id, 'frs_woo_product_tabs');
-		} elseif(!empty($tab_title) || !empty($tab_content)) {
+			delete_post_meta( $post_id, 'frs_woo_product_tabs' );
+		} elseif ( ! empty( $tab_title ) || ! empty( $tab_content ) ) {
 			$tab_data = array();
-			
+
 			$tab_id = '';
-			if($tab_title) {
+			if ( $tab_title ) {
 				// convert the tab title into an id string
-				$tab_id = strtolower($tab_title);
-				$tab_id = preg_replace("/[^\w\s]/",'',$tab_id);  // remove non-alphas, numbers, underscores or whitespace 
-				$tab_id = preg_replace("/_+/", ' ', $tab_id);	// replace all underscores with single spaces
-				$tab_id = preg_replace("/\s+/", '-', $tab_id);   // replace all multiple spaces with single dashes
-				$tab_id = 'tab-'.$tab_id;						// prepend with 'tab-' string
+				$tab_id = strtolower( $tab_title );
+				$tab_id = preg_replace( "/[^\w\s]/", '', $tab_id );
+				// remove non-alphas, numbers, underscores or whitespace
+				$tab_id = preg_replace( "/_+/", ' ', $tab_id );
+				// replace all underscores with single spaces
+				$tab_id = preg_replace( "/\s+/", '-', $tab_id );
+				// replace all multiple spaces with single dashes
+				$tab_id = 'tab-' . $tab_id;
+				// prepend with 'tab-' string
 			}
-			
+
 			// save the data to the database
-			$tab_data[] = array('title' => $tab_title,
-								'id' => $tab_id,
-								'content' => $tab_content);
-			update_post_meta($post_id, 'frs_woo_product_tabs', $tab_data);
+			$tab_data[] = array( 'title' => $tab_title, 'id' => $tab_id, 'content' => $tab_content );
+			update_post_meta( $post_id, 'frs_woo_product_tabs', $tab_data );
 		}
 	}
-	
+
+
+	private function woocommerce_wp_textarea_input( $field ) {
+		global $thepostid, $post;
+
+		if ( ! $thepostid ) $thepostid = $post->ID;
+		if ( ! isset( $field['placeholder'] ) ) $field['placeholder'] = '';
+		if ( ! isset( $field['class'] ) ) $field['class'] = 'short';
+		if ( ! isset( $field['value'] ) ) $field['value'] = get_post_meta( $thepostid, $field['id'], true );
+
+		echo '<p class="form-field ' . $field['id'] . '_field"><label style="display:block;" for="' . $field['id'] . '">' . $field['label'] . '</label><textarea class="' . $field['class'] . '" name="' . $field['id'] . '" id="' . $field['id'] . '" placeholder="' . $field['placeholder'] . '" rows="2" cols="20"' . (isset( $field['style'] ) ? ' style="' . $field['style'] . '"' : '') . '>' . esc_textarea( $field['value'] ) . '</textarea> ';
+
+		if ( isset( $field['description'] ) && $field['description'] )
+			echo '<span class="description">' . $field['description'] . '</span>';
+
+		echo '</p>';
+	}
+
+
+	/** Helper methods ******************************************************/
+
+
 	/**
-	 * Run every time since the activation hook is not executed when updating a plugin
+	 * Lazy-load the product_tabs meta data, and return true if it exists,
+	 * false otherwise
+	 *
+	 * @return true if there is custom tab data, false otherwise
+	 */
+	private function product_has_custom_tabs( $product ) {
+		if ( false === $this->tab_data ) {
+			$this->tab_data = maybe_unserialize( get_post_meta( $product->id, 'frs_woo_product_tabs', true ) );
+		}
+		// tab must at least have a title to exist
+		return ! empty( $this->tab_data ) && ! empty( $this->tab_data[0] ) && ! empty( $this->tab_data[0]['title'] );
+	}
+
+
+	/**
+	 * Checks if WooCommerce is active
+	 *
+	 * @since  1.0
+	 * @return bool true if WooCommerce is active, false otherwise
+	 */
+	public static function is_woocommerce_active() {
+
+		$active_plugins = (array) get_option( 'active_plugins', array() );
+
+		if ( is_multisite() )
+			$active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
+
+		return in_array( 'woocommerce/woocommerce.php', $active_plugins ) || array_key_exists( 'woocommerce/woocommerce.php', $active_plugins );
+	}
+
+
+	/** Lifecycle methods ******************************************************/
+
+
+	/**
+	 * Run every time.  Used since the activation hook is not executed when updating a plugin
 	 */
 	private function install() {
-		if(get_option('woocommerce_custom_product_tabs_lite_db_version') != WoocommerceCustomProductTabsLite::VERSION) {
-			$this->upgrade();
-			
-			// new version number
-			update_option('woocommerce_custom_product_tabs_lite_db_version', WoocommerceCustomProductTabsLite::VERSION);
-		}
-	}
-	
-	/**
-	 * Run when plugin version number changes
-	 */
-	private function upgrade() {
+
 		global $wpdb;
-		if(!get_option('woocommerce_custom_product_tabs_lite_db_version')) {
-			// this is one of the couple of original users who installed before I had a version option in the db
-			//  rename the post meta option 'product_tabs' to 'frs_woo_product_tabs'
-			$wpdb->query("UPDATE {$wpdb->postmeta} SET meta_key='frs_woo_product_tabs' WHERE meta_key='product_tabs';");
+
+		$installed_version = get_option( self::VERSION_OPTION_NAME );
+
+		// installed version lower than plugin version?
+		if ( -1 === version_compare( $installed_version, self::VERSION ) ) {
+			// new version number
+			update_option( self::VERSION_OPTION_NAME, self::VERSION );
 		}
 	}
-	
-	/**
-	 * Runs various functions when the plugin first activates (and every time
-	 * its activated after first being deactivated), and verifies that
-	 * the WooCommerce plugin is installed and active
-	 * 
-	 * @see register_activation_hook()
-	 * @link http://codex.wordpress.org/Function_Reference/register_activation_hook
-	 */
-	public static function on_activation() {
-		// checks if the woocommerce plugin is running and disables this plugin if it's not (and displays a message)
-		if (!is_plugin_active('woocommerce/woocommerce.php')) {
-			deactivate_plugins(plugin_basename(__FILE__));
-			wp_die(__('The WooCommerce Product Tabs Lite requires <a href="http://www.woothemes.com/woocommerce/" target="_blank">WooCommerce</a> to be activated in order to work. Please install and activate <a href="http://www.woothemes.com/woocommerce/" target="_blank">WooCommerce</a> first. <a href="'.admin_url('plugins.php').'"> <br> &laquo; Go Back</a>'));
-		}
-		
-		// set version number
-		update_option('woocommerce_custom_product_tabs_lite_db_version', WoocommerceCustomProductTabsLite::VERSION);
-	}
+
 }
-
-/**
- * instantiate class
- */
-$woocommerce_product_tabs_lite = new WoocommerceCustomProductTabsLite();
-
-endif; // class exists check
-
-/**
- * run the plugin activation hook
- */
-register_activation_hook(__FILE__, array('WoocommerceCustomProductTabsLite', 'on_activation'));
